025a6aa8200258ae372797b4dade6721b082ed4a
KiriamboFlex <kiriambo@bk.ru>
2023-01-17 16:52:49 +0500

    XZ chto delat!

--- app/src/main/java/com/example/drmpjct/MainActivity.java
+++ app/src/main/java/com/example/drmpjct/MainActivity.java
@@ -1,9 +1,25 @@
 package com.example.drmpjct;
+import static android.graphics.ImageFormat.*;
 
+import android.content.Context;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.ImageFormat;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.params.StreamConfigurationMap;
+
+import android.content.Context;
 import android.graphics.Point;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.view.Display;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -12,14 +28,59 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.drmpjct.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    public static final String LOG_TAG = "myLogs";
+    String[] myCameras = null;
+    private CameraManager mCameraManager = null;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
+
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+
+            myCameras = new String[mCameraManager.getCameraIdList().length];
+
+            for (String cameraID : mCameraManager.getCameraIdList()) {
+                Log.i(LOG_TAG, "cameraID: " + cameraID);
+                int id = Integer.parseInt(cameraID);
+
+                CameraCharacteristics cc = mCameraManager.getCameraCharacteristics(cameraID);
+                StreamConfigurationMap configurationMap = cc.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+
+                int Faceing = cc.get(CameraCharacteristics.LENS_FACING);
+
+                if (Faceing == CameraCharacteristics.LENS_FACING_BACK) {
+                    Log.i(LOG_TAG, "Camera with: ID " + cameraID + "is BACK CAMERA ");
+                }
+
+                Size[] sizesJPEG;
+                sizesJPEG = configurationMap.getHighSpeedVideoSizesFor(ImageFormat.JPEG);
+
+                if (sizesJPEG != null) {
+                    for (Size item : sizesJPEG) {
+                        Log.i(LOG_TAG, "w:" + item.getWidth() + " h:" + item.getHeight());
+                    }
+                } else {
+                    Log.i(LOG_TAG, "camera don't support JPEG");
+                }
+
+            }
+        } catch (CameraAccessException e) {
+            Log.e(LOG_TAG, e.getMessage());
+            e.printStackTrace();
+        }
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
